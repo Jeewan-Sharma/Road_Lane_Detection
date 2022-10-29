609ff2df2a108de419d04d59b7969c9895196380
Jeewan-Sharma <sjeewan53@gmail.com>
2022-10-29 17:00:22 +0545

    final commit

--- roadlane_images.py
+++ roadlane_images.py
@@ -42,6 +42,48 @@ def region_of_interest(img):
     return masked_image
 
 
+def make_coordinates(image, slope, intercept):
+    y1 = image.shape[0]  # height
+    y2 = int(y1*2/3)
+    x1 = int((y1-intercept)/slope)
+    x2 = int((y2-intercept)/slope)
+    print(y1, y2)
+    return np.array([x1, y1, x2, y2])
+
+
+def average_slope_intercept(img, lines):
+    left_fit = []
+    right_fit = []
+
+    # unmounting the ndarray
+    for line in lines:
+        x1, y1, x2, y2 = line.reshape(4)
+        parameters = np.polyfit((x1, x2), (y1, y2), 1)
+        slope = parameters[0]
+        intercept = parameters[1]
+
+        # dividing the left and right lane line using slope
+        if slope < 0:  # negative slope
+            left_fit.append((slope, intercept))
+        elif slope >= 0:  # positive slope
+            right_fit.append((slope, intercept))
+        continue
+
+    # calculating the avg value of slope and intercept
+    left_fit_average = np.average(left_fit, axis=0)
+    left_slope, left_intercept = left_fit_average[0], left_fit_average[1]
+
+    right_fit_average = np.average(right_fit, axis=0)
+
+    right_slope, right_intercept = right_fit_average[0], right_fit_average[1]
+
+    # making the coordinates of line to be drawn after avraging slope and intercept
+    left_line = make_coordinates(img, left_slope, left_intercept)
+    right_line = make_coordinates(img, right_slope, right_intercept)
+
+    return np.array([left_line, right_line])
+
+
 def display_lines(image, lines):
     mask = np.zeros_like(image)
     if lines is not None:
@@ -74,8 +116,12 @@ masked_image = region_of_interest(edgeDetectedImage)
 lines = cv2.HoughLinesP(masked_image, 2, np.pi/180, 100,
                         np.array([]), minLineLength=40, maxLineGap=5)
 
-# creating black images with the lines using hough probablistic transform
-line_image = display_lines(lane_image, lines)
+# avraging the length of lines
+
+averaged_lines = average_slope_intercept(lane_image, lines)
+
+# creating black images with the lines created earlier hough probablistic transform
+line_image = display_lines(lane_image, averaged_lines)
 
 # adding the masked_image and line_image
 lined_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 1)
